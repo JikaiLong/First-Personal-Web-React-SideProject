4778fb7d7e0f589c3d56be4c231e28d3661e9d98
Dev003 <you@example.com>
2017-12-06 17:21:25 -0500

    grammar fixed on description

--- src/component/Experience/experiencetree.js
+++ src/component/Experience/experiencetree.js
@@ -10,16 +10,16 @@ class Experience extends Component {
       <div id = "tree">
         <div className="vr">&nbsp;</div>
                 <li id = "li1" className="top">
-                <span id = "text"> Innovation Enginneer at CIBC - 2017 to 2018</span>
+                <span id = "text"> Innovation Engineer at CIBC - 2017 to 2018</span>
                 <div className="my_exlogo">
                   <img src={cibc} alt = ""/>
                   <br/>
                   <span>
                   <ul  className = "my_exdiscription">
                     <li>Build web apps with <span className = "focus"> Nodejs, Reactjs, Angularjs and Angular 4 </span> </li>
-                    <li>Query and Analyze facial scanner <span className = "focus">SQL database </span>to observe pattern for research</li>
+                    <li>Query and Analyze facial scanner <span className = "focus">SQL database </span>to observe patterns for research</li>
                     <li>Build IOS prototypes with Swift</li>
-                    <li>Play around with advanced tools such as 360 cameras (VR)</li>
+                    <li>Play around with tools such as 360 cameras (VR)</li>
                   </ul>
                   </span>
                 </div>
@@ -42,15 +42,15 @@ class Experience extends Component {
 
                 </li>
                 <li id = "li1" className="bottom">
-                <span id = "text"> Software Engineering Undergratuate at University of Toronto - 2017 to Now </span>
+                <span id = "text"> Software Engineering Undergraduate at University of Toronto - 2017 to Now </span>
                 <div className="my_exlogo">
                 <img src={uoft} alt = ""/>
                 <br/>
                 <span>
                 <ul  className = "my_exdiscription">
-                <li> - Study <span className = "focus">multiple Basic Programming Language and algorithms</span></li>
+                <li> - Study <span className = "focus">multiple Basic Programming Languages and algorithms</span></li>
                 <li> - Participate and Contribute in <span className = "focus">group projects and hackathons</span></li>
-                <li> - Exploration in different field</li>
+                <li> - Exploration in different fields other than CS</li>
                 <li> - Library all day every day</li>
                  </ul>
                  </span>
